000252f3ecd3249c8023672e702c63c70583df7a
rosechou <jcchou100@gmail.com>
2022-05-31 23:35:51 -0500

    Create discountPrices.py

---
+++ Weekly Contest 295/discountPrices.py
@@ -0,0 +1,7 @@
+class Solution:
+        def discountPrices(self, sentence: str, discount: int) -> str:
+            words = sentence.split()
+            for i, word in enumerate(words):
+                if word[0] == '$' and word[1 :].isnumeric():
+                    words[i] = f'${float(word[1 :]) * (1 - discount / 100):.2f}'
+            return ' '.join(words)
